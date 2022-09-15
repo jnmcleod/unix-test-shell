92adeb88ded5fb4f0e42f67438257ad9680218af
jnmcleod <113480362+jnmcleod@users.noreply.github.com>
2022-09-14 18:38:37 -0600

    Initial Commit

---
+++ unix-test-shell/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  unix-test-shell
+//
+//  Created by Jesse McLeod on 2022-09-14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
